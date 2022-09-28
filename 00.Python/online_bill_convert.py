00f3b2844442b0366f672b605ad87b3626824777
tpa <tp_proton_aly@protonmail.com>
2022-09-28 09:18:40 +0800

    'update bill convert python shell'

--- 00.Python/online_bill_convert.py
+++ 00.Python/online_bill_convert.py
@@ -9,7 +9,10 @@ from queue import Queue
 """
 using this python script to convert weixin xlsx file which convert by adobe_acrobat_dc_2022 from pdf files
 this file contains at most one year consumption situation, using this to covert xlsx to standard analysis csv file
+it also can covert csv data to standard data format for ssj data import to sqlite database
 """
+
+standard_format_data_key = ["交易号", "商家订单号", "交易创建时间", "付款时间", "最近修改时间", "交易对方", "商品名称", "金额", "收/支", "交易状态", "交易方式", "服务费", "备注", "资金状态", "数据来源"]
 
 
 def covert_weixin_xlsx_bill_to_csv(input_dirs=None):
@@ -217,6 +220,155 @@ def parseTradeID():
     print('----------------------------------------------------')
     print(data_set)
     print(len(data_set))
+
+
+def convert_alipay_bill_to_standard_data_format():
+    """
+    convert alipay csv data to standard format ssj data to import sqlite database
+    :return:
+    """
+    input_dir = '../f_input/'
+    files = os.listdir(input_dir)
+    standard_data_list = []
+    for f in files:
+        if f.startswith("alipay_record") and f.endswith(".csv"):
+            with open(f'{input_dir}{f}', 'r') as f:
+                reader = csv.reader(f)
+                data_header = []
+                data_read_flag = False
+                for row in reader:
+                    if row[0].strip().__eq__('交易号'):
+                        data_read_flag = True
+                        data_header = row
+                        continue
+                    if data_read_flag:
+                        if len(row) < 5:
+                            continue
+                        standard_data = dict.fromkeys(standard_format_data_key)
+                        column_index = 0
+                        while column_index < max(len(data_header), len(standard_format_data_key)):
+                            standard_data[data_header[column_index].strip()] = row[column_index].replace('\t', '').strip()
+                            column_index = column_index + 1
+                        standard_data['金额'] = standard_data['金额（元）']
+                        standard_data['服务费'] = standard_data['服务费（元）']
+                        standard_data.pop('金额（元）')
+                        standard_data.pop('服务费（元）')
+                        standard_data.pop('交易来源地')
+                        standard_data.pop('类型')
+                        standard_data.pop('成功退款（元）')
+                        standard_data['交易方式'] = '支付宝'
+                        if standard_data['资金状态'] is None:
+                            standard_data['资金状态'] = ' '
+                        if standard_data['备注'] is None:
+                            standard_data['备注'] = ' '
+                        standard_data['备注'] = f'{standard_data["备注"]}\n数据来源:支付宝'
+                        standard_data['数据来源'] = 'from_alipay_bill'
+                        standard_data_list.append(standard_data)
+    print(f'支付宝待插入数据{len(standard_data_list)}条')
+    return standard_data_list
+
+
+def convert_weixin_bill_to_standard_data_format():
+    """
+    convert
+    :return:
+    """
+    input_dir = '../f_input/'
+    files = os.listdir(input_dir)
+    standard_data_list = []
+    for f in files:
+        if f.startswith("微信支付账单") and f.endswith(".csv"):
+            with open(f'{input_dir}{f}', 'r', encoding="UTF-8") as f:
+                reader = csv.reader(f)
+                data_header = []
+                data_read_flag = False
+                for row in reader:
+                    if row[0].strip().__eq__('交易时间'):
+                        data_read_flag = True
+                        data_header = row
+                        continue
+                    if data_read_flag:
+                        if len(row) < 5:
+                            continue
+                        standard_data = dict.fromkeys(standard_format_data_key)
+                        column_index = 0
+                        while column_index < min(len(data_header), len(standard_format_data_key)):
+                            standard_data[data_header[column_index].strip()] = row[column_index].replace('\t', '').strip()
+                            column_index = column_index + 1
+                        standard_data['金额'] = standard_data['金额(元)'].replace('¥', '')
+                        standard_data['交易创建时间'] = standard_data['交易时间']
+                        standard_data['付款时间'] = standard_data['交易时间']
+                        standard_data['最近修改时间'] = standard_data['交易时间']
+                        standard_data['商品名称'] = standard_data['商品']
+                        standard_data['交易号'] = standard_data['交易单号']
+                        standard_data['商家订单号'] = standard_data['商户单号']
+                        standard_data['交易状态'] = '交易成功'
+                        standard_data['服务费'] = '0'
+
+                        pay_channel = standard_data['支付方式']
+                        if pay_channel.__contains__('银行'):
+                            standard_data['交易方式'] = '银行卡'
+                        else:
+                            standard_data['交易方式'] = '微信钱包'
+
+                        if standard_data['资金状态'] is None:
+                            standard_data['资金状态'] = ' '
+                        if standard_data['备注'] is None:
+                            standard_data['备注'] = ' '
+                        standard_data['备注'] = standard_data['备注'].replace('/', '')
+                        standard_data['备注'] = f'{standard_data["备注"]}\n{standard_data["当前状态"]}\n数据来源:微信短账单'
+                        standard_data['数据来源'] = 'from_weixin_bill'
+                        standard_data.pop('交易时间')
+                        standard_data.pop('商品')
+                        standard_data.pop('金额(元)')
+                        standard_data.pop('交易类型')
+                        standard_data.pop('交易单号')
+                        standard_data.pop('商户单号')
+                        standard_data.pop('支付方式')
+                        standard_data.pop('当前状态')
+                        standard_data_list.append(standard_data)
+    print(f'微信待插入数据{len(standard_data_list)}条')
+    # print(standard_data_list)
+    return standard_data_list
+
+
+def convert_weixin_long_bill_to_standard_data_format():
+    input_dir = '../f_input/'
+    files = os.listdir(input_dir)
+    standard_data_list = []
+    for f in files:
+        if f.startswith("standard_bill_from_weixin") and f.endswith(".csv"):
+            with open(f'{input_dir}{f}', 'r', encoding='UTF-8') as f:
+                reader = csv.reader(f)
+                data_header = []
+                is_first = True
+                for row in reader:
+                    if is_first:
+                        data_header = row
+                        is_first = False
+                        continue
+                    standard_data = dict.fromkeys(standard_format_data_key)
+                    column_index = 0
+                    while column_index < len(data_header):
+                        standard_data[data_header[column_index]] = row[column_index].replace('\t', '')
+                        column_index = column_index + 1
+                    if standard_data['备注'] is None:
+                        standard_data['备注'] = ' '
+                    standard_data['备注'] = f'{standard_data["备注"]}\n数据来源:微信支付'
+                    if standard_data['交易方式'] is not None:
+                        if standard_data['交易方式'].strip().__contains__('零钱'):
+                            standard_data['交易方式'] = '微信钱包'
+                        elif standard_data['交易方式'].strip().__contains__('银行'):
+                            standard_data['交易方式'] = '银行卡'
+                        elif standard_data['交易方式'].strip().__eq__('/'):
+                            standard_data['交易方式'] = '微信钱包'
+                    else:
+                        standard_data['交易方式'] = '微信钱包'
+                    standard_data['数据来源'] = 'from_weixin_bill'
+                    standard_data_list.append(standard_data)
+    print(f'微信待插入数据{len(standard_data_list)}条')
+    return standard_data_list
+    pass
 
 
 if __name__ == '__main__':
